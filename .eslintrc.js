b9b1209b238c376c98463dbd0ff48d3cffb9ed79
Murad Azimov <kalmahik@yahoo.com>
2021-12-06 16:58:29 +0300

    [MA][Challenge 1] Add required libraries

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  rules: {
+    "react-hooks/rules-of-hooks": 'error',
+    "react-hooks/exhaustive-deps": 'off',
+    "no-console": "error",
+  }
 };
